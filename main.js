a5c8f0126d267e839439ba032128f1155215bd12
Evandro Pomatti <evandro.pomatti@hbsis.com.br>
2020-05-01 01:03:36 -0300

    computed property

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
+        selectedVariant: 0,
         inventory: 100,
-        inStock: true,
         onSale: true,
         details: ["80% cotton", "20% polyester", "Genter-neutral"],
         variants: [
             {
                 id: 2234,
                 color: "green",
-                image: './assets/vmSocks-green-onWhite.jpg'
+                image: './assets/vmSocks-green-onWhite.jpg',
+                quantity: 10
             },
             {
                 id: 2235,
                 color: "blue",
-                image: './assets/vmSocks-blue-onWhite.jpg'
+                image: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -25,8 +27,20 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(image) {
-            this.image = image
+        updateProduct(index) {
+            this.selectedVariant = index
+        }
+    },
+    // Computed properties are cached
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
